320d60a46725019824c3da8070edb2dba15dcef0
cuteribs <85863433+cuteribs@users.noreply.github.com>
2025-08-20 18:28:26 +0800

    add demo projects for dapr packages

---
+++ samples/Dapr/HttpDemo/Program.cs
@@ -0,0 +1,48 @@
+using DNV.Dapr.Http;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+var configuration = builder.Configuration;
+
+var mockServer = "https://5692b783-d4d9-45a7-8dca-f49c30f31442.mock.pstmn.io";
+var clientName = "DAPR_CLIENT";
+
+services.AddHttpClient(clientName)
+	.AddHttpMessageHandler(() => new TestHandler());
+
+services.AddTransient<DaprHttpClient>(sp =>
+{
+	var httpClientFactory = sp.GetRequiredService<IHttpClientFactory>();
+	return new DefaultDaprHttpClient(httpClientFactory.CreateClient(clientName), new() { HttpPort = 3501 });
+});
+
+var app = builder.Build();
+
+app.MapGet("/dogs", (DaprHttpClient daprHttpClient) =>
+{
+	return daprHttpClient.InvokeMethodAsync<string>("GET", mockServer, "dogs");
+});
+
+app.MapPost("/dogs", (DaprHttpClient daprHttpClient, [FromBody] object breed) =>
+{
+	return daprHttpClient.InvokeMethodAsync<string>("POST", mockServer, "dogs", breed);
+});
+
+app.MapPost("/pubDog", (DaprHttpClient daprHttpClient, [FromBody] object dog) =>
+{
+	var metadata = new Dictionary<string, string> { { "pubMeta", "pub value" } };
+	return daprHttpClient.PublishEventAsync("pubsub", "dogtopic", dog, metadata);
+});
+
+app.Run();
+
+public class TestHandler : DelegatingHandler
+{
+	protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+	{
+		request.Headers.TryAddWithoutValidation("dapr-client", "default dapr http client");
+
+		return base.SendAsync(request, cancellationToken);
+	}
+}
---
+++ samples/Dapr/PubSubDemo/Program.cs
@@ -0,0 +1,85 @@
+using Dapr.Client;
+using DNV.Dapr.PubSub;
+using DNV.Dapr.PubSub.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+var configuration = builder.Configuration;
+
+var clientName = "PUBSUB_CLIENT";
+
+services.AddHttpClient(clientName)
+	.AddHttpMessageHandler(() => new PubSubHandler());
+
+services.AddDaprPubSubClient(
+	clientName,
+	o =>
+	{
+		o.PubSubName = "pubsub";
+		o.TopicName = "testtopic";
+	},
+	o => o.HttpPort = 3501
+);
+
+var app = builder.Build();
+
+app.UseCloudEvents();
+app.MapSubscribeHandler();
+
+app.MapPost("/httppub", async (IDaprPubSubClientFactory factory, [FromBody] object message) =>
+{
+	var metadata = new Dictionary<string, string>
+	{
+		{ "pubMeta", "pub value" }
+	};
+	var client = factory.CreateClient(clientName);
+	await client.PublishAsync(message, metadata);
+});
+
+app.MapPost("/sdkpub", async ([FromBody] object message) =>
+{
+	var metadata = new Dictionary<string, string>
+	{
+		{ "pubMeta", "pub value" }
+	};
+	var client = new DaprClientBuilder()
+		.UseHttpEndpoint("http://localhost:3501")
+		.Build();
+	await client.PublishEventAsync("pubsub", "testtopic", message, metadata);
+});
+
+app.MapPost(
+	"/sub",
+	(HttpContext context, object message) =>
+	{
+		Console.WriteLine($"============================= Received message =============================");
+
+		var headers = context.Request.Headers;
+
+		foreach (var header in headers)
+		{
+			Console.WriteLine($"{header.Key}: {header.Value}");
+		}
+
+		Console.WriteLine(message);
+		return Results.Ok();
+	}
+).WithTopic("pubsub", "testtopic");
+
+app.Run();
+
+
+public class PubSubHandler : DelegatingHandler
+{
+	protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+	{
+		var uriBuilder = new UriBuilder(request.RequestUri!);
+		var query = System.Web.HttpUtility.ParseQueryString(uriBuilder.Query);
+		query["metadata.injectedMeta"] = "injected value";
+		uriBuilder.Query = query.ToString();
+		request.RequestUri = uriBuilder.Uri;
+
+		return base.SendAsync(request, cancellationToken);
+	}
+}
